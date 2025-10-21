ce580134b1f7a54f69909ebfa11d476565fcfc67
nscan7777-del <nscan7777@gmail.com>
2025-10-21 04:27:50 -0300

    Add SolanaWalletProvider component for wallet integration

---
+++ src/providers/WalletProvider.tsx
@@ -0,0 +1,25 @@
+import React, { useMemo } from "react";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+type Props = { children: React.ReactNode };
+const network = "mainnet-beta";
+
+export default function SolanaWalletProvider({ children }: Props) {
+  const endpoint = useMemo(() => clusterApiUrl(network as any), []);
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
